b547e025ec0b8f1da14914bd04cd2986e4909843
MuriloDamaceno <damacenomurilo01@gmail.com>
2026-05-12 09:41:35 -0300

    usuarioModel.js

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -11,3 +11,5 @@ usuarioSchema.pre('save', async function () {
     if (!this.isModified('senha')) return;
     this.senha = await bcrypt.hash(this.senha, 10);
 });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
