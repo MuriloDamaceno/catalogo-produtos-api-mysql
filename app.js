215386a7901d7fbb15df3c0487c58bde80954639
MuriloDamaceno <damacenomurilo01@gmail.com>
2026-06-02 09:07:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const conectarDB = require('./src/config/db');
-
 const authRoutes = require('./src/routes/authRoutes');
 const produtoRoutes = require('./src/routes/produtoRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Conecta ao banco
 conectarDB();
@@ -17,7 +19,32 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/produtos', produtoRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Minha API Simples',
+      version: '1.0.0',
+      description: 'Exemplo de API básica com Swagger',
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: ['./app.js'],
+};
+
+const specs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+// Endpoint de exemplo
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Olá, mundo!' });
 });
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
